30b3a5cf1ecf67b7a26609c9b4767ee6f5b6af88
e9mgund <mayur.gund@enine.school>
2024-06-07 20:57:55 +0530

    Task is completed

--- script2.py
+++ script2.py
@@ -2,46 +2,34 @@ import os
 import datetime
 import pandas as pd
 import json
-
 import calendar
-date = datetime.datetime(2020,1,1)
-start_year = 2020
-total_days = 1
-delta = datetime.timedelta(days=1)
 
-while start_year < 2025 :
-    if start_year == 2024 :
-        for j in range(1,6):
-            summary = []
-            os.makedirs(str(start_year)+"/"+str(start_year)+str(j).zfill(2))
-            fp = open(str(start_year)+"/"+str(start_year)+str(j).zfill(2)+'/'+'totals.json','w')
-            for k in range(1,calendar.monthrange(date.year,date.month)[1]+1) :
-                out_frame = pd.DataFrame(columns=['date','resource','value'])
-                for file in os.scandir('/home/kilo/playground/task/generated_files') :
-                    df = pd.read_csv(str(file.path))
-                    df = df.iloc[24*total_days-24:24*total_days]
-                    out_frame = pd.concat([out_frame,df],ignore_index=True)
-                summary.append({'date':date,'sum':out_frame['value'].sum()})
-                filename = str(date.year)+str(date.month).zfill(2)+str(date.day).zfill(2)+'.csv'
-                out_frame.to_csv(str(start_year)+"/"+str(start_year)+str(j).zfill(2)+'/'+filename,index=False)
-                date += delta
-                total_days += 1
-            json.dump(summary,fp,indent=6,default=str)
-    else :
-        for j in range(1,13):
-            summary = []
-            os.makedirs(str(start_year)+"/"+str(start_year)+str(j).zfill(2))
-            fp = open(str(start_year)+"/"+str(start_year)+str(j).zfill(2)+'/'+'totals.json','w')
-            for k in range(1,calendar.monthrange(date.year,date.month)[1]+1) :
-                out_frame = pd.DataFrame(columns=['date','resource','value'])
-                for file in os.scandir('/home/kilo/playground/task/generated_files') :
-                    df = pd.read_csv(str(file.path))
-                    df = df.iloc[24*total_days-24:24*total_days]
-                    out_frame = pd.concat([out_frame,df],ignore_index=True)
-                summary.append({'date':date,'sum':out_frame['value'].sum()})
-                filename = str(date.year)+str(date.month).zfill(2)+str(date.day).zfill(2)+'.csv'
-                out_frame.to_csv(str(start_year)+"/"+str(start_year)+str(j).zfill(2)+'/'+filename,index=False)
-                date += delta
-                total_days += 1
-            json.dump(summary,fp,indent=6,default=str)
-    start_year += 1
+out_dir = 'processed'
+os.mkdir(out_dir)
+
+#path for the generated files in the first task
+gen_path = '/home/kilo/playground/task/generated_files'
+
+for file in os.scandir(gen_path):
+    fp = open(file.path)
+    data = fp.readlines()
+    resource_name = data[1].strip().split(',')[1]
+    start_date = datetime.datetime(year=2020,month=1,day=1)
+    end_date = datetime.datetime(year=2024,month=5,day=31,hour=23)
+    delta = datetime.timedelta(days=1)
+    totals = []
+    for i in range(1,len(data),24):
+        dir_path = os.path.join(out_dir,resource_name,str(start_date.year),str(start_date.year)+str(start_date.month).zfill(2))
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(os.path.join(dir_path,str(start_date.year)+str(start_date.month).zfill(2)+str(start_date.day).zfill(2)+'.csv'),'w') as out :
+            out.write('date,resource,value\n')
+            out.writelines(data[i:i+24])
+            day_sum = sum([float(i.strip().split(',')[-1]) for i in data[i:i+24]])
+            totals.append({'date':start_date,'sum':day_sum})
+        if start_date.day == calendar.monthrange(start_date.year,start_date.month)[1] :
+            with open(os.path.join(dir_path,'totals.json'),'w') as jp:
+                json.dump(totals,jp,indent=6,default=str)
+            totals = []
+        start_date += delta
+    fp.close()
